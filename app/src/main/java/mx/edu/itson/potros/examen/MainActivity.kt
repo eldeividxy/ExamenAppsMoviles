6e41c1cc5eac564ae4d54a004fbbe370174eba51
eldeividxy <deividsotelo02@gmail.com>
2024-02-13 15:40:32 -0800

    Examenv1

--- app/src/main/java/mx/edu/itson/potros/examen/MainActivity.kt
+++ app/src/main/java/mx/edu/itson/potros/examen/MainActivity.kt
@@ -2,10 +2,54 @@ package mx.edu.itson.potros.examen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val cc:TextView= findViewById(R.id.etC)
+        val cf:TextView= findViewById(R.id.etF)
+        val btnGC: Button=findViewById(R.id.btnGC)
+        val btnGF: Button=findViewById(R.id.btnGF)
+
+        btnGC.setOnClickListener {
+            var gradosF: Double=0.0
+            try{
+                gradosF=cf.text.toString().toDouble()
+            } catch (ex: java.lang.Exception){
+
+            }
+
+            var gradosC=convertirGC(gradosF)
+            val formatted ="%.2f".format(gradosC)
+            cc.setText(formatted)
+        }
+
+        btnGF.setOnClickListener {
+            var gradosC: Double=0.0
+            try{
+                gradosC=cc.text.toString().toDouble()
+            } catch (ex: java.lang.Exception){
+
+            }
+
+            var gradosF=convertirGF(gradosC)
+            val formatted ="%.2f".format(gradosC)
+        }
+
+
+    }
+    fun convertirGC(gradosF: Double):Double{
+        return (gradosF-32)/1.8
+    }
+    fun convertirGF(gradosC: Double):Double{
+        return (gradosC*1.8)+32;
     }
 }
